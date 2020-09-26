39f481563f303be1c5eaee3c4e5a8888e3069528
Damian <damiandanielvigo@gmail.com>
2020-09-25 23:51:19 -0300

    front-done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
 import React from 'react';
-import logo from './logo.svg';
+import { loadStripe } from '@stripe/stripe-js';
+import { Elements, CardElement, useStripe, useElements } from '@stripe/react-stripe-js';
+
+import 'bootswatch/dist/lux/bootstrap.min.css';
 import './App.css';
+
+const stripePromise = loadStripe(
+  'pk_test_51HRgkqGUDDhTw3nvqdIk2qgCWxjIzzb59d4BbUZzqyyjUjL2LOjqq7YH9npQWJsIVmqJKTvbCKJTmoORQJzEWeUX00sZeE2ulI'
+);
+
+const CheckoutForm = () => {
+  const stripe = useStripe(),
+    elements = useElements();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const { error, paymentMethod } = await stripe.createPaymentMethod({
+      type: 'card',
+      card: elements.getElement(CardElement),
+    });
+
+    if (!error) {
+      console.log(paymentMethod);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className='card card-body'>
+      <img
+        src='https://cdn.shopify.com/s/files/1/2981/4874/products/HERO-min_1024x1024.png?v=1548633006'
+        alt='anne-pro'
+        className='img-fluid'
+      />
+
+      <div className='form-group'>
+        <CardElement className='form-control' />
+      </div>
+      <button className='btn btn-success'>Buy</button>
+    </form>
+  );
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Elements stripe={stripePromise}>
+      <div className='container p-4'>
+        <div className='row'>
+          <div className='col-md-4 offset-md-4'>
+            <CheckoutForm />
+          </div>
+        </div>
+      </div>
+    </Elements>
   );
 }
 
